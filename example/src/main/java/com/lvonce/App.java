cda316ea538980aec31624c3984bb61a7d767c73
Wang Wei <thegenius@vip.qq.com>
2017-08-23 13:20:42 +0800

    update example

--- example/src/main/java/com/lvonce/App.java
+++ example/src/main/java/com/lvonce/App.java
@@ -4,12 +4,12 @@ import static com.lvonce.hermes.EntityFactory.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+		IFoo foo1 = create(IFoo.class, "com.lvonce.JavaFoo");
+		IFoo foo2 = create(IFoo.class, "com.lvonce.KotlinFoo");
+		IFoo foo3 = create(IFoo.class, "com.lvonce.GroovyFoo");
 		Runnable func = new Runnable() {
 			public void run() {
 				while (true) {
-					IFoo foo1 = create(IFoo.class, "com.lvonce.JavaFoo");
-					IFoo foo2 = create(IFoo.class, "com.lvonce.KotlinFoo");
-					IFoo foo3 = create(IFoo.class, "com.lvonce.GroovyFoo");
 					System.out.println(foo1.hello("msg"));
 					System.out.println(foo2.hello("msg"));
 					System.out.println(foo3.hello("msg"));
